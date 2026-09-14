3218e5d775b29c2f912a7fecc7fe255d2b5cf7ce
Instinct (assistant for Siddhant) <v4o3k7@mail.instinct.com>
2026-09-14 05:49:07 +0530

    Preserve Contributor Quest heading in generator

--- .github/quest/generate.py
+++ .github/quest/generate.py
@@ -230,7 +230,7 @@ def render_section(prs):
     nav = " &nbsp;·&nbsp;\n".join(
         f'<a href="#user-content-xp-{p}"><b>{KANJI[p]} the way of the {p}</b> · {counts[p]}</a>'
         for p in ORDER) + "<br>"
-    parts = [f"""## My Contribution Quest
+    parts = [f"""## Contributor Quest
 
 <details name="xp" open>
 <summary><b>༄ the crossroads</b> - a contribution quest</summary>
